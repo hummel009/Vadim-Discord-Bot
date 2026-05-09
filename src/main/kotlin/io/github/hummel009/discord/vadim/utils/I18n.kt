91250e04d3315fddab4e664e387cc6caf93aa8bd
Yahor Burbouski <hummel.turbamentum@gmail.com>
2026-05-09 19:27:44 +0300

    Update I18n.kt

--- src/main/kotlin/io/github/hummel009/discord/vadim/utils/I18n.kt
+++ src/main/kotlin/io/github/hummel009/discord/vadim/utils/I18n.kt
@@ -1,12 +1,12 @@
 package io.github.hummel009.discord.vadim.utils
 
+import com.google.gson.reflect.TypeToken
 import io.github.hummel009.discord.vadim.bean.GuildData
 import java.io.InputStreamReader
 
 object I18n {
 	private val cache: MutableMap<String, Map<String, String>> = mutableMapOf()
 
-	@Suppress("UNCHECKED_CAST")
 	fun of(key: String, guildData: GuildData): String {
 		val lang = guildData.lang
 		val translations = cache.getOrPut(lang) {
@@ -21,10 +21,10 @@ object I18n {
 			val inputStream = this::class.java.classLoader.getResourceAsStream("assets/lang/$langFileName")!!
 
 			InputStreamReader(inputStream, Charsets.UTF_8).use {
-				gson.fromJson(it, Map::class.java) as Map<String, String>
+				gson.fromJson(it, object : TypeToken<Map<String, String>>() {}.type)
 			}
 		}
 
-		return translations[key] ?: "Invalid translation key!"
+		return translations[key] ?: "Invalid translation key: $key"
 	}
 }
